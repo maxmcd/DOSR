567428d9eafa704cb7748ddd688b456dd6470172
NathanEpstein <ne2210@columbia.edu>
2015-01-15 13:36:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#About
+
+A desktop REPL application build on NW.js and AngularJS currently supporting javascript, python, and R.
+
+(Disclaimer: This application is still under development.)
+
+
+#Getting Started
+
+1) git clone this repo
+2) cd into this repo
+3) `bower install angular#1.2.28`
+4) `git clone https://github.com/codemirror/CodeMirror.git`
---
+++ controllers/main.js
@@ -0,0 +1,40 @@
+angular.module('repl',[])
+      .controller('mainCtrl', ['$scope',function mainCtrl($scope){
+        //boolean, do we show the repl view?
+        $scope.repling = false;
+        //objects for supported languages
+        $scope.langs = [{name:'Javscript', suffix:'.js',command:'node'},{name:'R',suffix:'.R', command:'Rscript'}, {name:'Python', suffix:'.py', command:'python'}];
+
+        //function to select language
+        $scope.chooseLang = function(arg){
+          $scope.lang = this.langs[arg];
+          $scope.mirror = CodeMirror(document.getElementById('input'), {
+            lineNumbers:true,
+            mode: $scope.lang.name.toLowerCase()
+          });
+          $scope.repling = true;
+        };
+
+        $scope.back = function(){
+          $scope.output = '';
+          $scope.repling = false;
+          //remove the mirror from the DOM instead
+          $scope.mirror.getWrapperElement().remove()
+        };
+
+        $scope.submit = function(){
+          var fs = require('fs');
+          var exec = require('child_process').exec;
+
+          var fileName = 'scripts/myScript'+this.lang.suffix;
+          var command = this.lang.command+' '+fileName;
+          var input = $scope.mirror.getDoc().getValue();
+
+          fs.writeFile(fileName,input,function(){
+            var child = exec(command,function(err,res){
+              $scope.output = 'RESPONSE: '+res+'\n'+'ERROR: '+err;
+              $scope.$apply();
+            });
+          });
+        };
+      }]);
